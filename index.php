f68ed6ce864fb032c4bb76aca5f7a47560596233
StephanieWillekens <r0298534@student.thomasmore.be>
2016-05-09 20:56:54 +0200

    HTML & CSS
    Register.php, login.php, forgetPassword.php -> formulieren aangemaakt,
    klaargezet, opmaak geoptimaliseerd
    
    Index.php -> opmaak
    
    Profile.php -> opmaak (nog niet 100% af)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Date: 17/03/16
  * Time: 16:32
  */
-
+    session_start();
 ?>
 
 <!doctype html>
@@ -17,12 +17,79 @@
 </head>
 <body>
 
-<?php include_once ("header.inc.php");?>
+<header>
 
-<div id="posts">
-    <!-- posts -->
-    <p>HIER KOMEN POSTS</p>
-</div>
+    <a href="index.php">
+        <img id="logo2" src="images/logo.png" alt="">
+    </a>
+    <div id="search">
+        <form action="get">
+            <input id="searchText" type="search" name="search" placeholder="Search">
+        </form>
+    </div>
+    <div id="profile">
+        <a href="profile.php">
+            <img id="logoProfile" src="images/logoProfile.png" alt="">
+        </a>
+    </div>
+
+</header>
+
+<!-- Hier moet een for each loop komen om alle updates te tonen -->
+<section id="updates">
+    <article id="updatesArticle">
+        <!-- profielfoto, profielnaam, time -->
+        <div id="article1">
+            <!-- profielfoto -->
+            <!-- DIT IS EEN TESTAFBEELDING -->
+            <img id="profilePic" src="images/test.png" alt="">
+            <!-- naam profiel -->
+            <a class="link" id="profileName" href="">name profile</a>
+            <!-- tijdstip upload -->
+            <p id="time">time</p>
+        </div>
+
+        <!-- gepostte foto -->
+        <div id="article2">
+            <!-- DIT IS EEN TESTAFBEELDING!-->
+            <img id="upload" src="images/test.png" alt="">
+        </div>
+
+        <!-- namen personen die geliked hebben -->
+        <div id="article3">
+            <!-- likes -->
+            <a class="link" id="likeProfileName" href="">name profile like</a>
+            <p id="description"> like this.</p>
+            <!-- naam profiel -->
+        </div>
+
+        <!-- profielnaam, beschrijving post -->
+        <div id="article4">
+            <a class="link" id= "profileName" href="">name profile</a>
+            <!-- beschrijving post -->
+            <p id="description">description post</p>
+        </div>
+
+        <!-- like icoon, comment, report -->
+        <div id="article5">
+            <!-- like icon -->
+            <!-- MOET REALTIME MET AJAX! -->
+            <a id="likeIcon" href=""></a>
+
+            <!-- MOET REALTIME MET AJAX! -->
+            <form id="comment" action="">
+                <input id="commentText" type="text" name="comment" placeholder="Add a comment...">
+            </form>
+        </div>
+
+        <div id="article6">
+            <a id="report" href="">Report inappropriate</a>
+        </div>
+
+    </article>
+
+</section>
+
 
 </body>
 </html>
